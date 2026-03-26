9765cadc59894506043d48d0951ef4cb0f51cb3d
rafaelcarterbusiness@gmail.com <rafaelcarterbusiness@gmail.com>
2026-03-26 02:45:18 -0400

    - Added swagger and seed data into the project

--- DripCampaignTracker/Program.cs
+++ DripCampaignTracker/Program.cs
@@ -1,3 +1,6 @@
+using DripCampaignTracker.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,18 +9,40 @@ builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseInMemoryDatabase("DripCampaignTrackerDb"));
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("FrontEnd", policy =>
+    {
+        policy.WithOrigins("http://localhost:3000") //Change to what front end port it will be
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
+builder.Services.AddSwaggerGen();
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var context = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    context.Database.EnsureCreated();
+    DbSeeder.Seed(context);
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
+app.UseCors("Frontend");
 app.MapControllers();
 
 app.Run();
